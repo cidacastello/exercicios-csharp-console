0cefbb276dde30e6d926d69332f95a8c85f62f99
Cida Castello <56411540+cidacastello@users.noreply.github.com>
2020-06-22 20:05:07 -0300

    Create -Ex-01-circunferencia
    Exercício 01 - circunferência

---
+++ exercicios_codigo_fonte/Ex-01-Circunferencia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex_01_Circunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração das variáveis
+            double raio, area, comprimento;
+            Console.Write("Informe o valor do raio: ");
+
+            //valor do PI 3.14159
+            raio = Convert.ToDouble( Console.ReadLine());
+
+            area = 3.14159 * raio * raio;
+
+            comprimento = 2 * 3.14159 * raio;
+
+            Console.WriteLine($"Area = {area} e comprimento = {comprimento}");
+            Console.WriteLine("Pressione uma tecla para sair");
+            Console.ReadKey(true);
+
+        }
+    }
+}
