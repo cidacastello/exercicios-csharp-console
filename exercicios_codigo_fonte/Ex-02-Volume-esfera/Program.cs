871134fc903a011c9112df8e50e3cd0a0f3ab528
Cida Castello <56411540+cidacastello@users.noreply.github.com>
2020-06-23 08:44:17 -0300

    Upload Ex-02-Volume esfera
    Upload ex-02 volume da esfera

---
+++ exercicios_codigo_fonte/Ex-02-Volume-esfera/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex_02_Volume_esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declaração das variáveis
+            double volume, raio;
+            Console.Write("Informe o valor do raio: ");
+
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            volume = 4.0/3.0 * 3.14159 * raio * raio * raio;
+
+            Console.WriteLine($"Volume da esfera: {volume}");
+
+            Console.WriteLine("Digite uma tecla para continuar!");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
