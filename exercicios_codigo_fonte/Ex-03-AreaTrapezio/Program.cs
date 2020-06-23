0ed27e8adf850692859b3e95ac549fdaa7c2ef35
Cida Castello <56411540+cidacastello@users.noreply.github.com>
2020-06-23 19:59:42 -0300

    upload ex-03-area trapezio
    upload ex-03-area trapezio

---
+++ exercicios_codigo_fonte/Ex-03-AreaTrapezio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex_03_AreaTrapezio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração das variáveis
+            double baseMenor, baseMaior, altura, area;
+
+            //entrada de dados
+            Console.Write("Informe o valor da base maior: ");
+            baseMaior = Convert.ToDouble(Console.ReadLine());
+
+             Console.Write("Informe o valor da base menor: ");
+            baseMenor = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe o valor da altura: ");
+            altura=Convert.ToDouble(Console.ReadLine());
+
+            //processamento
+            area = ( (baseMaior + baseMenor)/2)*altura;
+
+            //saída - resposta do problema
+            Console.WriteLine("Area do trapezio: " +   area);
+
+            Console.WriteLine("Digite uma tecla para continuar!");
+
+            Console.ReadKey(true);
+            
+        }
+    }
+}
